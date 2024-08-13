4d4d0e019ab2e8a28587356dbc721f4ed54de6cb
waynetanjingheng <wtjh00@gmail.com>
2024-08-13 14:56:52 +0800

    feat: add util fns and constructor impl for TaskScheduler header

--- include/TaskScheduler.hpp
+++ include/TaskScheduler.hpp
@@ -6,23 +6,46 @@
 
 class TaskScheduler {
 public:
-    explicit TaskScheduler(size_t numWorkerThreads);
+    explicit TaskScheduler(size_t numWorkerThreads) : _stop(false), _tasksInExecutionCount(0) {
+        // spawn threads in the pool and execute tasks in the queue
+        for (size_t i = 0; i < _tasks.size(); i++) {
+            _workers.emplace_back([this] { this->executeTasksFromQueue(); });
+        }
+    };
 
-    ~TaskScheduler(); // ensure graceful exit by terminating all worker threads
+    virtual ~TaskScheduler(); // ensure graceful exit by terminating all worker threads
 
     void enqueueTask(const std::function<void()> &task);
 
-    void waitForAllTasksToComplete();
+    virtual void waitForAllTasksToComplete();
+
+    size_t getTasksInExecutionCount() const {
+        return _tasksInExecutionCount.load();
+    }
+
+    size_t getTasksInQueueCount() const {
+        return _tasks.size();
+    }
 
 private:
     std::vector<std::thread> _workers; // worker thread pool
     std::queue<std::function<void()> > _tasks;
-    std::mutex _queueMutex;
+    std::mutex _taskQueueMutex;
     std::condition_variable _condition;
     std::atomic<bool> _stop; // flag to stop all worker threads
-    std::atomic<size_t> _activeTasks;
+    std::atomic<size_t> _tasksInExecutionCount;
 
-    void executeTasksFromQueue();
+    virtual void executeTasksFromQueue() = 0;
+
+    bool isTaskQueueEmpty() const {
+        return _tasks.empty();
+    }
+
+    std::function<void()> &getNextTaskInQueue() {
+        std::function<void()> &task = _tasks.front();
+        _tasks.pop();
+        return task;
+    }
 };
 
 
